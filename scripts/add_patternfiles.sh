948826bee41cd81d4a0ad3ff837742319aa4cce2
Dominik Lindner <d.lindner@dundee.ac.uk>
2018-11-08 13:39:47 +0000

    Use paths in pattern files

--- scripts/add_patternfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp 180712_H2B_22ss_Courtney1_20180712-163837_p00_c00_preview.pattern /uod/idr/filesets/idr0051-fulton-tailbudlightsheet/20181025-ftp/Dataset_3/Unprocessed\ Image\ Files/
-cp 180712_H2B_22ss_Courtney_p00_c00_reg_preview.klb.pattern /uod/idr/filesets/idr0051-fulton-tailbudlightsheet/20181025-ftp/Dataset_3/Processed\ Image\ Files/
-cp 2018-06-28_21ss_DMSO_TF_20180628-185945_p00_c00_reg_preview.pattern /uod/idr/filesets/idr0051-fulton-tailbudlightsheet/20181025-ftp/Dataset_2/Unprocessed\ Image\ Files/
-cp embryo_dmso_2_new_17-00-44_p00_c00_reg_preview.pattern /uod/idr/filesets/idr0051-fulton-tailbudlightsheet/20181025-ftp/Dataset_1/Unprocessed\ Image\ Files/
-cp embryo_dmso_2_new_17-00-44_p00_c00_reg_preview.klb.pattern /uod/idr/filesets/idr0051-fulton-tailbudlightsheet/20181025-ftp/Dataset_1/Processed\ Image\ Files/
